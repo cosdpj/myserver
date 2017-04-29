342e3ca6243f7bb5ebae1aec6844e90934e770ff
cosdpj <dpjcos@q.com>
2017-04-29 11:09:03 -0600

    Initial

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express'),
+  app = express(),
+  port = process.env.PORT || 3000,
+  bodyParser = require('body-parser');
+
+app.use(bodyParser.json({ type: 'application/json' }));
+
+app.get('/list', function (req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });
+})
+
+app.post('/add', function (req, res) {
+     debugger;
+       var data = req.body;
+       console.log( data );
+       res.end( JSON.stringify(data));
+
+})
+
+app.listen(port);
+
+console.log('RESTful API server started on: ' + port);
